91ee4c1a9ffbc40d3596ee3f93cddad3eedee2ed
ramkrushna <ramkrushnaprabhu@gmail.com>
2022-12-06 19:56:43 +0530

    third commit

--- index.js
+++ index.js
@@ -21,19 +21,19 @@ app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
 //-----------------------------deployment-------------------------------
-const __dirname1 = path.resolve();
+// const __dirname1 = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/FrontEnd/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname1, "/FrontEnd/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname1, "FrontEnd", "build", "index.html"));
-  });
-} else {
-  app.get("/", (req, res) => {
-    res.send("API Running");
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname1, "FrontEnd", "build", "index.html"));
+//   });
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("API Running");
+//   });
+// }
 //-----------------------------deployment-------------------------------
 
 app.use(notFound);
